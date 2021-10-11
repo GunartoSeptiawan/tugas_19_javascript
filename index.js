288e7a845433cb56f54ee899c9daaf7c36257b86
Gunarto Septiawan <gunartosept@gmail.com>
2021-10-11 16:08:51 +0700

    tugas_19_javascript
    Tugas Niomic

---
+++ index.js
@@ -0,0 +1,18 @@
+function Repeat(){
+  var kalimat = "Semangat...";
+  var bintang = "*****"
+  console.log(kalimat.repeat(5));
+  console.log(bintang.repeat(1));
+  console.log(bintang.repeat(4));
+  console.log(bintang.repeat(8));
+  console.log(bintang.repeat(12));
+  console.log(bintang.repeat(16));
+  console.log(bintang.repeat(20));
+  console.log(bintang.repeat(16));
+  console.log(bintang.repeat(12));
+  console.log(bintang.repeat(8));
+  console.log(bintang.repeat(4));
+  console.log(bintang.repeat(1));
+}
+
+Repeat();
